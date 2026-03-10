46cdf7eabc21a820baa3b4b1403484bddb0c70a3
SHAH Khalid <kugdadag14972@gmail.com>
2026-03-10 20:34:18 +0500

    Add files via upload

---
+++ lab 2/Lab work Task 1.cpp
@@ -0,0 +1,12 @@
+// Program run hoga lekin koi output print nahi karega
+
+#include <iostream>      // input output library
+using namespace std;     // standard namespace use karne ke liye
+
+int main()               // main function jahan se program start hota hai
+{
+
+    // yahan koi cout nahi hai is liye screen par kuch print nahi hoga
+
+    return 0;            // program end
+}
