3762d2e4c845be7a9cbc62b231b2028fbd881732
SHAH Khalid <kugdadag14972@gmail.com>
2026-04-20 21:16:45 +0500

    Add files via upload

---
+++ LAB 05/TASKS/LAB 05 TASK 03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int num, i = 1, sum = 0;
+ cout << "Enter a positive integer: ";
+ cin >> num;
+ while(i <= num) {
+ sum += i;
+ i++;
+ }
+ cout << "Sum = " << sum;
+ return 0;
+}
