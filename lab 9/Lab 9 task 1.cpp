25fc77f2844b0be0ec0d6d1e20be6717ad32a98b
SHAH Khalid <kugdadag14972@gmail.com>
2026-05-11 22:21:22 +0500

    Add files via upload

---
+++ lab 9/Lab 9 task 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>   // Input output library ko include karna
+using namespace std;  // Standard namespace use karna
+
+// greet function ko declare karna
+void greet()
+{
+    // Greeting message display karna
+    cout << "Welcome to C++ Programming!" << endl;
+}
+
+int main()
+{
+    // greet function ko call karna
+    greet();
+
+    return 0; // Program successful end
+}
