09fc88c29b445d63464bde740451cd235154751f
SHAH Khalid <kugdadag14972@gmail.com>
2026-05-12 23:16:57 +0500

    Add files via upload

---
+++ lab 11/lab 11 task 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // String variable declare aur initialize
+    string message = "Welcome to UET Peshawar";
+
+    // String ki length display karna
+    cout << "Message: " << message << endl;
+    cout << "Length of string: " << message.length() << endl;
+
+    return 0;
+}
